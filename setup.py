490a6733d55cbebd8a7ddf8a8a5f4a98ee21cea6
Ian Bicking <ian@ianbicking.org>
2007-08-11 18:29:58 +0000

    Initial layout

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='FlatAtomPub',
+      version=version,
+      description="Simple implementation of AtomPub (aka Atom Publishing Protocol)",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='atom atompub app wsgi web',
+      author='Ian Bicking',
+      author_email='ianb@colorstudy.com',
+      url='',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'WebOb',
+        'WebTest',
+      ],
+      dependency_links=[
+        'http://svn.pythonpaste.org/Paste/WebOb/trunk#egg=WebOb-dev',
+        'http://svn.pythonpaste.org/Paste/WebTest/trunk#egg=WebTest-dev',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [paste.app_factory]
+      main = flatatompub.wsgiapp:make_app
+      """,
+      )
